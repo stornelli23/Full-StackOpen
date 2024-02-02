48aa29b4219eec6a29d5fef07fdb7005f47d469b
Stornelli23 <santystornelli@hotmail.com>
2024-02-02 19:55:55 +0100

    Exercise 1.6 to 1.14 already done. Part1 finished

---
+++ Part1/anecdotes/src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
+
+  const randomAnecdote = ()=>{
+    const randomNumber = Math.floor(Math.random() * anecdotes.length);
+    setSelected(randomNumber)
+    }
+
+  const handleVotes = () => {
+    const newVotes = [...votes];
+    newVotes[selected]++;
+    setVotes(newVotes);
+  }
+
+  const maxVotesIndex = votes.indexOf(Math.max(...votes));
+
+  return (
+    <div>
+      <div>
+         <h2>Anecdote of the day</h2>
+         <p>{anecdotes[selected]}</p>
+         <p>has {votes[selected]} votes</p>
+      </div>
+      <button onClick={handleVotes}>vote</button>
+      <button onClick={randomAnecdote}>next anecdote</button>
+      <div>
+        <h2>Anecdote with most votes</h2>
+        <p>{anecdotes[maxVotesIndex]}</p>
+        <p>has {votes[maxVotesIndex]} votes</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
--- Part1/unicafe/src/App.jsx
+++ Part1/unicafe/src/App.jsx
@@ -1,49 +1,86 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
-const Display = (props) => {
+const Button = ({ onClick, text }) => {
   return (
-    <div>{props.counter}</div>
-  )
-}
+    <button onClick={onClick}>{text}</button>
+  );
+};
 
-const Button = (props) => {
+const StatisticLine = ({ text, value }) => {
   return (
-    <button onClick={props.onClick}>
-      {props.text}
-    </button>
+    <table>
+      <tbody>
+        <tr>
+          <td width="60px">{text}</td>
+          <td>{value}</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+
+const Statistics = ({good, neutral, bad, totalFeedback, averageScore, positivePercentage}) => {
+  return (
+    <div>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="all" value={totalFeedback} />
+          <StatisticLine text="average" value={averageScore} />
+          <StatisticLine text="positive" value={`${positivePercentage} %`} />
+    </div>
   )
 }
 
 const App = () => {
-  const [counter, setCounter] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  console.log('rendering with counter value', counter)
+  const handleGoodClick = () => {
+    const updatedGood = good + 1;
+    setGood(updatedGood);
+  };
 
-  const increaseByOne = () => {
+  const handleNeutralClick = () => {
+    const updatedNeutral = neutral + 1;
+    setNeutral(updatedNeutral);
+  };
 
-    console.log('increasing, value before', counter)
-    setCounter(counter + 1)
-  }
+  const handleBadClick = () => {
+    const updatedBad = bad + 1;
+    setBad(updatedBad);
+  };
 
-  const decreaseByOne = () => { 
-
-    console.log('decreasing, value before', counter)
-    setCounter(counter - 1)
-  }
-
-  const setToZero = () => {
-
-    console.log('resetting to zero, value before', counter)
-    setCounter(0)
-  }
+  const totalFeedback = good + neutral + bad;
+  const averageScore = (good - bad) / totalFeedback || 0;
+  const positivePercentage = (good / totalFeedback) * 100 || 0;
 
   return (
     <div>
-      <Display counter={counter} />
-      <Button onClick={increaseByOne} text="plus" />
-      <Button onClick={setToZero} text="zero" />
-      <Button onClick={decreaseByOne} text="minus" />
+
+      <h1>Give feedback</h1>
+      <Button onClick={handleGoodClick} text="good" />
+      <Button onClick={handleNeutralClick} text="neutral" />
+      <Button onClick={handleBadClick} text="bad" />
+
+      <h2>Statistics</h2>
+      {totalFeedback > 0 
+      ?
+      (<Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        totalFeedback={totalFeedback}
+        averageScore={averageScore}
+        positivePercentage={positivePercentage}
+      />) 
+      : (<p>No feedback given</p>)
+    }
+
+
     </div>
-  )
-} 
-export default App
+  );
+};
+
+export default App;
