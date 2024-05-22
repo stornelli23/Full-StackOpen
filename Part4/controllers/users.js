93122b580745101807188b62391f32dc9de6b1cf
Stornelli23 <santystornelli@hotmail.com>
2024-05-22 23:16:14 +0200

    Delete and put methods added on users controller

--- Part4/controllers/users.js
+++ Part4/controllers/users.js
@@ -11,6 +11,7 @@ usersRouter.get('/', async (request, response) => {
       url: 1
       })
 
+      
   response.json(users)
 })
 
@@ -43,5 +44,50 @@ usersRouter.post('/', async (request, response) => {
   
     response.status(201).json(savedUser)
   })
+
+usersRouter.put('/:id', async (request, response) => {
+  const { id } = request.params;
+  const { username, name, password } = request.body;
+
+  if (!username || !password) {
+    return response.status(400).json({ error: 'Both username and password must be provided' });
+  }
+
+  if (username.length < 3 || password.length < 3) {
+    return response.status(400).json({ error: 'Username and password must be at least 3 characters long' });
+  }
+
+  const existingUser = await User.findOne({ username });
+  if (existingUser && existingUser._id.toString() !== id) {
+    return response.status(400).json({ error: 'Username must be unique' });
+  }
+
+  const saltRounds = 10;
+  const passwordHash = await bcrypt.hash(password, saltRounds);
+
+  const updatedUser = await User.findByIdAndUpdate(
+    id,
+    { username, name, passwordHash },
+    { new: true, runValidators: true, context: 'query' }
+  );
+
+  if (!updatedUser) {
+    return response.status(404).json({ error: 'User not found' });
+  }
+
+  response.json(updatedUser);
+});
+
+usersRouter.delete('/:id', async (request, response) => {
+  const { id } = request.params;
+
+  const deletedUser = await User.findByIdAndDelete(id);
+
+  if (!deletedUser) {
+    return response.status(404).json({ error: 'User not found' });
+  }
+
+  response.status(204).end();
+});
 
 module.exports = usersRouter
